0cd7388ff1b74c7813b48d5625803a97fb98fc1d
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:31:36 +0300

    Added have been CategoryController

---
+++ RealEstate.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.Api.Dtos.CategoryDtos;
+using RealEstate.Api.Repositories.CategoryRepository;
+
+namespace RealEstate.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoriesController(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CategoryList()
+    {
+        var categories = await _categoryRepository.GetAllCategoryAsync();
+        return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> CategoryById(int id)
+    {
+        var category = await _categoryRepository.GetCategoryAsync(id);
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public IActionResult CreateCategory(CreateCategoryDto categoryDto)
+    {
+        _categoryRepository.CreateCategory(categoryDto);
+        return Ok("Kategori Başarılı Bir Şekilde Eklendi!");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteCategory(int id)
+    {
+        _categoryRepository.DeleteCategory(id);
+        return Ok("Kategori Başarılı Bir Şekilde Silindi!");
+
+    }
+
+    [HttpPut]
+    public IActionResult UpdateCategory(UpdateCategoryDto categoryDto)
+    {
+        _categoryRepository.UpdateCategory(categoryDto);
+        return Ok("Kategori Başarılı Bir Şekilde Güncellendi!");
+    }
+}
