d3518728236ca7bb186a4a0562fbbab5cf377918
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:16:52 +0300

    Added have been ResultCategoryDto for listing

---
+++ RealEstate.Api/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate.Api.Dtos.CategoryDtos;
+
+public class ResultCategoryDto
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public bool Status { get; set; }
+}
