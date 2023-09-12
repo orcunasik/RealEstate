00f2094df368eb96e8d3bc6e52745f1600a63acd
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:42:18 +0300

    Added have been IoC services for Context, CategoryRepositoy and ProductRepository classes

--- RealEstate.Api/Program.cs
+++ RealEstate.Api/Program.cs
@@ -1,15 +1,18 @@
+using RealEstate.Api.Models.DapperContext;
+using RealEstate.Api.Repositories.CategoryRepository;
+using RealEstate.Api.Repositories.ProductRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<BaseContext>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
