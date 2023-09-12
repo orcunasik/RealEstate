84dd9c1ed06e652dad67511645bb96d49b3223dc
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:33:46 +0300

    Added have been ResultProductDto class for listing

---
+++ RealEstate.Api/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Api.Dtos.ProductDtos;
+
+public class ResultProductDto
+{
+    public int ProductId { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string City { get; set; }
+    public string District { get; set; }
+    public int CategoryId { get; set; }
+}
