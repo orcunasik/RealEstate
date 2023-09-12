940b07cac322d6b9b8fac5ddfa149bd7b1c2f794
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:36:15 +0300

    Added have been ResultProductWithCategoryDto class for listing product with category

---
+++ RealEstate.Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Api.Dtos.ProductDtos;
+
+public class ResultProductWithCategoryDto
+{
+    public int ProductId { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string City { get; set; }
+    public string District { get; set; }
+    public string CategoryName { get; set; }
+}
