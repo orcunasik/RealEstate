14b68dd30216603951cf2aa77b41f88c8a3d2ee8
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:21:24 +0300

    GetByIdCategoryDto class to fetch by id added have been

---
+++ RealEstate.Api/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate.Api.Dtos.CategoryDtos;
+
+public class GetByIdCategoryDto
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public bool Status { get; set; }
+}
