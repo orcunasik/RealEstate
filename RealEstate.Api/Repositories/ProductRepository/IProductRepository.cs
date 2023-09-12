091d982b9060f3ffdf04e7c8289e7cbbb41e3cab
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:37:31 +0300

    Added have been Product Repository with interface and concrete class

---
+++ RealEstate.Api/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate.Api.Dtos.ProductDtos;
+
+namespace RealEstate.Api.Repositories.ProductRepository;
+
+public interface IProductRepository
+{
+    Task<List<ResultProductDto>> GetAllProductAsync();
+    Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync();
+}
---
+++ RealEstate.Api/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using RealEstate.Api.Dtos.ProductDtos;
+using RealEstate.Api.Models.DapperContext;
+
+namespace RealEstate.Api.Repositories.ProductRepository;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly BaseContext _context;
+
+    public ProductRepository(BaseContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<ResultProductDto>> GetAllProductAsync()
+    {
+        string query = "Select * From Products";
+        using (var connection = _context.CreateConnection())
+        {
+            var products = await connection.QueryAsync<ResultProductDto>(query);
+            return products.ToList();
+        }
+    }
+
+    public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+    {
+        string query = "Select ProductId,Title,Price,City,District,CategoryName From Products inner join Categories on Products.CategoryId = Categories.CategoryId";
+        using (var connection = _context.CreateConnection())
+        {
+            var products = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+            return products.ToList();
+        }
+    }
+}
