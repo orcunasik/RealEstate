cc6c191543efb93cc67cc3c73e3e26b752b28642
Orçun Aşık <asikorcun@gmail.com>
2023-09-12 18:14:46 +0300

    Added have been Context class

---
+++ RealEstate.Api/Models/DapperContext/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate.Api.Models.DapperContext;
+
+public class BaseContext
+{
+    private readonly IConfiguration _configuraiton;
+    private readonly string _connectionString;
+
+    public BaseContext(IConfiguration configuraiton)
+    {
+        _configuraiton = configuraiton;
+        _connectionString = _configuraiton.GetConnectionString("connection");
+    }
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
