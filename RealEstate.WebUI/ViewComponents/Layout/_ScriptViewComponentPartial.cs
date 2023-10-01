a177ce3ec23a2514bc70802ebeead13d90655814
Orçun Aşık <asikorcun@gmail.com>
2023-10-01 18:22:37 +0300

    ViewComponent structure was created for the DefaultLayout of the homepage

---
+++ RealEstate.WebUI/ViewComponents/Layout/_ScriptViewComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.WebUI.ViewComponents.Layout;
+
+public class _ScriptViewComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
