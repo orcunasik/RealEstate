d90ee425af4605cf6ab6be9c911ab8921efdf2c1
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 18:59:02 +0300

    Added have been ServiceDtos for CRUD operations

---
+++ RealEstate.Api/Dtos/ServiceDtos/GetByIdServiceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate.Api.Dtos.ServiceDtos;
+
+public class GetByIdServiceDto
+{
+    public int ServiceId { get; set; }
+    public string ServiceName { get; set; }
+    public bool ServiceStatus { get; set; }
+}
