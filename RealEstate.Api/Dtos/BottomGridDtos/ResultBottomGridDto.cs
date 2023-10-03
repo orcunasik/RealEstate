898ac8b0e7bcb4a750064f24fab40a58f975c75e
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 21:06:36 +0300

    Added have been BottomGridDtos for Crud operations.

---
+++ RealEstate.Api/Dtos/BottomGridDtos/ResultBottomGridDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Api.Dtos.BottomGridDtos;
+
+public class ResultBottomGridDto
+{
+    public int BottomGridId { get; set; }
+    public string Icon { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
