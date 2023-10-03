a9e7747fbc9c0e204cb2a41530b5e22ef8824bef
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 15:52:13 +0300

    Added have been WhoWeAreDetailDtos for CRUD operations

---
+++ RealEstate.Api/Dtos/WhoWeAreDetailDtos/CreateWhoWeAreDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Api.Dtos.WhoWeAreDetailDtos;
+
+public class CreateWhoWeAreDetailDto
+{
+    public string Title { get; set; }
+    public string Subtitle { get; set; }
+    public string Description1 { get; set; }
+    public string Description2 { get; set; }
+}
