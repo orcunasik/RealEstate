6ac52c1df9ea54b0239706f70171775aded3370b
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 18:25:29 +0300

    removed

--- RealEstate.WebUI/Dtos/WhoWeAreDetailDtos/ResultServiceDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RealEstate.WebUI.Dtos.WhoWeAreDetailDtos;
-
-public class ResultServiceDto
-{
-    public int ServiceId { get; set; }
-    public string ServiceName { get; set; }
-    public bool ServiceStatus { get; set; }
-}
