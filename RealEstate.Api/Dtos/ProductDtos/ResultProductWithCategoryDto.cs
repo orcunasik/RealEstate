39b4336701b1be30e3a6332255545902cf92d246
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 16:36:23 +0300

    Type, CoverImage and Address properties have been added.

--- RealEstate.Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
+++ RealEstate.Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -8,4 +8,7 @@ public class ResultProductWithCategoryDto
     public string City { get; set; }
     public string District { get; set; }
     public string CategoryName { get; set; }
+    public string Type { get; set; }
+    public string CoverImage { get; set; }
+    public string Address { get; set; }
 }
