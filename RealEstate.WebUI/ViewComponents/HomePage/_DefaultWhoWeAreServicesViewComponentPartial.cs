99fde32146f58430e4101b391c0b913bddce07f5
Orçun Aşık <asikorcun@gmail.com>
2023-10-03 20:06:47 +0300

    Added have been a new ViewComponent for WhoWeAreServices

---
+++ RealEstate.WebUI/ViewComponents/HomePage/_DefaultWhoWeAreServicesViewComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.WebUI.Dtos.ServiceDtos;
+
+namespace RealEstate.WebUI.ViewComponents.HomePage;
+
+public class _DefaultWhoWeAreServicesViewComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public _DefaultWhoWeAreServicesViewComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Services/");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
