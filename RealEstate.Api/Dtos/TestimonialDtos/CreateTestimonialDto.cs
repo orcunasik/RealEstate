27f9d91997a5045f665a3b40e963dca58779b499
Orçun Aşık <asikorcun@gmail.com>
2023-10-04 20:57:03 +0300

    Added have been TestimonialDtos for Crud Operations

---
+++ RealEstate.Api/Dtos/TestimonialDtos/CreateTestimonialDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate.Api.Dtos.TestimonialDtos;
+
+public class CreateTestimonialDto
+{
+    public string NameSurname { get; set; }
+    public string Title { get; set; }
+    public string Comment { get; set; }
+    public bool Status { get; set; }
+}
---
+++ RealEstate.Api/Dtos/TestimonialDtos/UpdateTestimonialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.Api.Dtos.TestimonialDtos;
+
+public class UpdateTestimonialDto
+{
+    public int TestimonialId { get; set; }
+    public string NameSurname { get; set; }
+    public string Title { get; set; }
+    public string Comment { get; set; }
+    public bool Status { get; set; }
+}
