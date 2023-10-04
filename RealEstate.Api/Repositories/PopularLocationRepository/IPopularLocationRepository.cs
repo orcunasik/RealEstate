26b4d1929a757c7c1d752c1843243e70324befb7
Orçun Aşık <asikorcun@gmail.com>
2023-10-04 13:52:13 +0300

    Added have been interface and class for PopularLocationRepository

---
+++ RealEstate.Api/Repositories/PopularLocationRepository/IPopularLocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate.Api.Dtos.PopularLocationDtos;
+
+namespace RealEstate.Api.Repositories.PopularLocationRepository;
+
+public interface IPopularLocationRepository
+{
+    Task<List<ResultPopularLocationDto>> GetAllPopularLocationsAsync();
+    Task<GetByIdPopularLocationDto> GetPopularLocationAsync(int id);
+    void CreatePopularLocation(CreatePopularLocationDto popularLocationDto);
+    void UpdatePopularLocation(UpdatePopularLocationDto popularLocationDto);
+    void DeletePopularLocation(int id);
+}
