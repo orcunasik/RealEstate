872cfa96f593fdfe8270c393df8c83a77d417926
Orçun Aşık <asikorcun@gmail.com>
2023-10-04 14:15:40 +0300

    Has been edited to consume the PopularLocations api.

--- RealEstate.WebUI/ViewComponents/HomePage/_DefaultProductListExploreCitiesViewComponentPartial.cs
+++ RealEstate.WebUI/ViewComponents/HomePage/_DefaultProductListExploreCitiesViewComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.WebUI.Dtos.PopularLocationDtos;
 
 namespace RealEstate.WebUI.ViewComponents.HomePage;
 
 public class _DefaultProductListExploreCitiesViewComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultProductListExploreCitiesViewComponentPartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/PopularLocations/");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
