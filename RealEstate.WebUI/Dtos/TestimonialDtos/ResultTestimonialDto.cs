ec17d006b9b48a35bc39badf590fc4a086424ae3
Orçun Aşık <asikorcun@gmail.com>
2023-10-04 21:13:26 +0300

    Added have been TestimonialDto for OurClientsViewComponent

---
+++ RealEstate.WebUI/Dtos/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.WebUI.Dtos.TestimonialDtos;
+
+public class ResultTestimonialDto
+{
+    public int TestimonialId { get; set; }
+    public string NameSurname { get; set; }
+    public string Title { get; set; }
+    public string Comment { get; set; }
+    public bool Status { get; set; }
+}
