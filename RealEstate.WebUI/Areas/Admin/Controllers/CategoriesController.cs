aee37adf8e36ce87cddf9ac3194e13178fc2863d
Orçun Aşık <asikorcun@gmail.com>
2023-10-12 00:42:09 +0300

    Category Api was edited to consume

--- RealEstate.WebUI/Areas/Admin/Controllers/CategoriesController.cs
+++ RealEstate.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.WebUI.Dtos.CategoryDtos;
 
 namespace RealEstate.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoriesController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Categories/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
