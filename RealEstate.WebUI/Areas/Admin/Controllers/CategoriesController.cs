aeca7b26a84fc0a4a4e06e46cb751b6c531a9253
Orçun Aşık <asikorcun@gmail.com>
2023-10-12 17:29:02 +0300

    Added CreateCategory method and View page

--- RealEstate.WebUI/Areas/Admin/Controllers/CategoriesController.cs
+++ RealEstate.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,29 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate.WebUI.Dtos.CategoryDtos;
+using System.Text;
 
-namespace RealEstate.WebUI.Areas.Admin.Controllers
+namespace RealEstate.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoriesController : Controller
 {
-    [Area("Admin")]
-    public class CategoriesController : Controller
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-        public CategoriesController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-        public async Task<IActionResult> Index()
-        {
-            HttpClient client = _httpClientFactory.CreateClient();
-            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Categories/");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                string jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
+    public CategoriesController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
     }
+    public async Task<IActionResult> Index()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Categories/");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateCategory()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(CreateCategoryDto categoryDto)
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        string jsonData = JsonConvert.SerializeObject(categoryDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7201/api/Categories/", stringContent);
+        if (responseMessage.IsSuccessStatusCode)
+            return RedirectToAction("Index");
+        return View();
+    }
+
 }
