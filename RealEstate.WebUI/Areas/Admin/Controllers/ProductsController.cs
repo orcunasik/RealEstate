7654357311cf2f3bfd968061ab0e2c1523d787b9
Orçun Aşık <asikorcun@gmail.com>
2023-10-22 21:30:44 +0300

    Added have been ProdcutsController and Index page

---
+++ RealEstate.WebUI/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.WebUI.Dtos.ProductDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ProductsController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IActionResult> Index()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Products/ProductListWithCategory");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string jsonData = await responseMessage.Content.ReadAsStringAsync();
+            List<ResultProductDto> values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+        }
+        return View();
+    }
+}
