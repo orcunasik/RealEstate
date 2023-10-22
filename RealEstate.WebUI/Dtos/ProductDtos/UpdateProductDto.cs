72690ade8399f8023b40632d7f07c28f9eb0da40
Orçun Aşık <asikorcun@gmail.com>
2023-10-22 21:29:44 +0300

    Added have been dto classes for Create and Update operations

---
+++ RealEstate.WebUI/Dtos/ProductDtos/UpdateProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate.WebUI.Dtos.ProductDtos;
+
+public class UpdateProductDto
+{
+    public int ProductId { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string City { get; set; }
+    public string District { get; set; }
+    public string CategoryName { get; set; }
+    public string Type { get; set; }
+    public string CoverImage { get; set; }
+    public string Address { get; set; }
+}
