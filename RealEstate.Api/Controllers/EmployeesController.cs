8d9a9e8fc4570ce3b94eb4db0213606886e4dad4
Orçun Aşık <asikorcun@gmail.com>
2023-10-23 14:04:59 +0300

    Added have been EmployeeController

---
+++ RealEstate.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.Api.Dtos.EmployeeDtos;
+using RealEstate.Api.Repositories.EmployeeRepository;
+
+namespace RealEstate.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeesController(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> EmployeeList()
+    {
+        var employees = await _employeeRepository.GetAllEmployeeAsync();
+        return Ok(employees);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> EmployeeById(int id)
+    {
+        var employee = await _employeeRepository.GetEmployeeAsync(id);
+        return Ok(employee);
+    }
+
+    [HttpPost]
+    public IActionResult CreateEmployee(CreateEmployeeDto employeeDto)
+    {
+        _employeeRepository.CreateEmployee(employeeDto);
+        return Ok("Personel Başarılı Bir Şekilde Eklendi!");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteEmployee(int id)
+    {
+        _employeeRepository.DeleteEmployee(id);
+        return Ok("Personel Başarılı Bir Şekilde Silindi!");
+
+    }
+
+    [HttpPut]
+    public IActionResult UpdateEmployee(UpdateEmployeeDto employeeDto)
+    {
+        _employeeRepository.UpdateEmployee(employeeDto);
+        return Ok("Personel Başarılı Bir Şekilde Güncellendi!");
+    }
+}
