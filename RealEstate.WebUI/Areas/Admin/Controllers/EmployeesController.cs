3a9e666fe95dc8b068967770adaca5495e4fc273
Orçun Aşık <asikorcun@gmail.com>
2023-10-23 16:03:30 +0300

    Added have been EmployeeController and Index page

---
+++ RealEstate.WebUI/Areas/Admin/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.WebUI.Dtos.EmployeeDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.Controllers;
+
+public class EmployeesController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public EmployeesController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:7201/api/Employees/");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            string jsonData = await responseMessage.Content.ReadAsStringAsync();
+            List<ResultEmployeeDto> employees = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+            return View(employees);
+        }
+        return View();
+    }
+}
