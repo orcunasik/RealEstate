efe713e97bb039b412ae747dc9f503ee86fe16f8
Orçun Aşık <asikorcun@gmail.com>
2023-10-23 14:03:11 +0300

    Added have been Dto classes for Employee CRUD operations

---
+++ RealEstate.Api/Dtos/EmployeeDtos/CreateEmployeeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Api.Dtos.EmployeeDtos;
+
+public class CreateEmployeeDto
+{
+    public string Name { get; set;}
+    public string Title { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string ImageUrl { get; set; }
+    public bool Status { get; set; }
+}
