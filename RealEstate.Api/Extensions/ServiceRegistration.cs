213b8eaaa06fb3e590526a68d93c56047e136332
Orçun Aşık <asikorcun@gmail.com>
2023-11-04 01:19:46 +0300

    All DI services have collected in a static method and declared to IoC.

---
+++ RealEstate.Api/Extensions/ServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using RealEstate.Api.Models.DapperContext;
+using RealEstate.Api.Repositories.BottomGridRepository;
+using RealEstate.Api.Repositories.CategoryRepository;
+using RealEstate.Api.Repositories.EmployeeRepository;
+using RealEstate.Api.Repositories.PopularLocationRepository;
+using RealEstate.Api.Repositories.ProductRepository;
+using RealEstate.Api.Repositories.ServiceRepository;
+using RealEstate.Api.Repositories.StatisticsRepository;
+using RealEstate.Api.Repositories.TestimonialRepository;
+using RealEstate.Api.Repositories.WhoWeAreDetailRepository;
+
+namespace RealEstate.Api.Extensions;
+
+public static class ServiceRegistration
+{
+    public static void LoadMyRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<BaseContext>();
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IProductRepository, ProductRepository>();
+        services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+        services.AddTransient<IServiceRepository, ServiceRepository>();
+        services.AddTransient<IStatisticsRepository, StatisticsRepository>();
+        services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+        services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+        services.AddTransient<ITestimonialRepository, TestimonialRepository>();
+        services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+    }
+}
--- RealEstate.Api/Program.cs
+++ RealEstate.Api/Program.cs
@@ -1,27 +1,11 @@
-using RealEstate.Api.Models.DapperContext;
-using RealEstate.Api.Repositories.BottomGridRepository;
-using RealEstate.Api.Repositories.CategoryRepository;
-using RealEstate.Api.Repositories.EmployeeRepository;
-using RealEstate.Api.Repositories.PopularLocationRepository;
-using RealEstate.Api.Repositories.ProductRepository;
-using RealEstate.Api.Repositories.ServiceRepository;
-using RealEstate.Api.Repositories.TestimonialRepository;
-using RealEstate.Api.Repositories.WhoWeAreDetailRepository;
+using RealEstate.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<BaseContext>();
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
-builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+builder.Services.LoadMyRepositories();
 
 var app = builder.Build();
 
