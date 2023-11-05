749656cee3a7dc40c15f1c3454972a5531163276
Orçun Aşık <asikorcun@gmail.com>
2023-11-05 21:39:55 +0300

    A service registration class has been added to notify the IoC about DI services collectively.

---
+++ RealEstate.WebUI/Configurations/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.ApiClients.Concretes;
+
+namespace RealEstate.WebUI.Configurations;
+
+public static class ServiceRegistration
+{
+    public static void LoadMyServices(this IServiceCollection services)
+    {
+        services.AddHttpClient();
+
+        services.AddTransient<IDomainService, DomainService>();
+
+        services.AddTransient<ICategoryApiClient, CategoryApiClient>();
+        services.AddTransient<IEmployeeApiClient, EmployeeApiClient>();
+        services.AddTransient<IServiceApiClient, ServiceApiClient>();
+        services.AddTransient<IWhoWeAreApiClient, WhoWeAreApiClient>();
+        services.AddTransient<IStatisticsApiClient, StatisticsApiClient>();
+    }
+}
