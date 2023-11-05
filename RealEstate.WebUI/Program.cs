595b35fe53c7753d23ea80abed10b26ec9f60481
Orçun Aşık <asikorcun@gmail.com>
2023-11-05 21:40:54 +0300

    Created LoadMyServices() method has been added.

--- RealEstate.WebUI/Program.cs
+++ RealEstate.WebUI/Program.cs
@@ -1,16 +1,15 @@
+using RealEstate.WebUI.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient();
+builder.Services.LoadMyServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
