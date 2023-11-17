0c77da0d9fe817b8de6c59c7ab7864d442e67e5a
Orçun Aşık <asikorcun@gmail.com>
2023-11-17 21:23:18 +0300

    Added interfaces and associated implementations to make requests to the API.

---
+++ RealEstate.WebUI/ApiClients/Abstracts/ITestimonialApiClient.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate.WebUI.Dtos.CategoryDtos;
+using RealEstate.WebUI.Dtos.TestimonialDtos;
+
+namespace RealEstate.WebUI.ApiClients.Abstracts;
+
+public interface ITestimonialApiClient
+{
+    Task<List<ResultTestimonialDto>> GetAllAsync();
+    Task<UpdateTestimonialDto> GetUpdateAsync(int id);
+    Task<CreateTestimonialDto> AddAsync(CreateTestimonialDto testimonialDto);
+    Task<bool> UpdateAsync(UpdateTestimonialDto testimonialDto);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ RealEstate.WebUI/ApiClients/Concretes/TestimonialApiClient.cs
@@ -0,0 +1,52 @@
+﻿using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Configurations;
+using RealEstate.WebUI.Dtos.TestimonialDtos;
+
+namespace RealEstate.WebUI.ApiClients.Concretes;
+
+public class TestimonialApiClient : ITestimonialApiClient
+{
+    private readonly IHttpClientFactory _httpClient;
+    private readonly IDomainService _domainService;
+    public TestimonialApiClient(IHttpClientFactory httpClient, IDomainService domainService)
+    {
+        _httpClient = httpClient;
+        _domainService = domainService;
+    }
+    public async Task<CreateTestimonialDto> AddAsync(CreateTestimonialDto testimonialDto)
+    {
+        HttpClient client = _httpClient.CreateClient();
+        HttpResponseMessage response = await client.PostAsJsonAsync(_domainService.Domain() + "api/Testimonials", testimonialDto);
+        if (response.IsSuccessStatusCode)
+            return await response.Content.ReadFromJsonAsync<CreateTestimonialDto>();
+        return null;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        HttpClient client = _httpClient.CreateClient();
+        HttpResponseMessage response = await client.DeleteAsync(_domainService.Domain() + $"api/Testimonials/{id}");
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<List<ResultTestimonialDto>> GetAllAsync()
+    {
+        HttpClient client = _httpClient.CreateClient();
+        List<ResultTestimonialDto>? response = await client.GetFromJsonAsync<List<ResultTestimonialDto>>(_domainService.Domain() + "api/Testimonials");
+        return response;
+    }
+
+    public async Task<UpdateTestimonialDto> GetUpdateAsync(int id)
+    {
+        HttpClient client = _httpClient.CreateClient();
+        UpdateTestimonialDto? response = await client.GetFromJsonAsync<UpdateTestimonialDto>(_domainService.Domain() + $"api/Testimonials/{id}");
+        return response;
+    }
+
+    public async Task<bool> UpdateAsync(UpdateTestimonialDto testimonialDto)
+    {
+        HttpClient client = _httpClient.CreateClient();
+        HttpResponseMessage response = await client.PutAsJsonAsync(_domainService.Domain() + "api/Testimonials/", testimonialDto);
+        return response.IsSuccessStatusCode;
+    }
+}
