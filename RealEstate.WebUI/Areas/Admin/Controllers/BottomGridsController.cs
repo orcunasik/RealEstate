84d16075d536e7e522fb7e7da833f91dd75215e4
Orçun Aşık <asikorcun@gmail.com>
2023-11-17 21:24:41 +0300

    Controller has been added for Crud operations

---
+++ RealEstate.WebUI/Areas/Admin/Controllers/BottomGridsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Dtos.BottomGridDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class BottomGridsController : Controller
+{
+    private readonly IBottomGridApiClient _apiClient;
+
+    public BottomGridsController(IBottomGridApiClient bottomGridApiClient)
+    {
+        _apiClient = bottomGridApiClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<ResultBottomGridDto> bottomGrids = await _apiClient.GetAllAsync();
+        if (bottomGrids is not null)
+            return View(bottomGrids);
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateBottomGrid()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDto bottomGridDto)
+    {
+        await _apiClient.AddAsync(bottomGridDto);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> DeleteBottomGrid(int id)
+    {
+        bool isSuccess = await _apiClient.DeleteAsync(id);
+        if (isSuccess)
+            return RedirectToAction(nameof(Index));
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateBottomGrid(int id)
+    {
+        UpdateBottomGridDto bottomGrid = await _apiClient.GetUpdateAsync(id);
+        if (bottomGrid is not null)
+            return View(bottomGrid);
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateBottomGrid(UpdateBottomGridDto bottomGridDto)
+    {
+        bool isSuccess = await _apiClient.UpdateAsync(bottomGridDto);
+        if (isSuccess)
+            return RedirectToAction(nameof(Index));
+        return View();
+    }
+}
---
+++ RealEstate.WebUI/Areas/Admin/Controllers/PopularLocationsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Dtos.PopularLocationDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class PopularLocationsController : Controller
+{
+    private readonly IPopularLocationApiClient _apiClient;
+
+    public PopularLocationsController(IPopularLocationApiClient popularLocationApiClient)
+    {
+        _apiClient = popularLocationApiClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        List<ResultPopularLocationDto> popularLocations = await _apiClient.GetAllAsync();
+        if (popularLocations is not null)
+            return View(popularLocations);
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreatePopularLocation()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDto popularLocationDto)
+    {
+        await _apiClient.AddAsync(popularLocationDto);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> DeletePopularLocation(int id)
+    {
+        bool isSuccess = await _apiClient.DeleteAsync(id);
+        if (isSuccess)
+            return RedirectToAction(nameof(Index));
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdatePopularLocation(int id)
+    {
+        UpdatePopularLocationDto popularLocation = await _apiClient.GetUpdateAsync(id);
+        if (popularLocation is not null)
+            return View(popularLocation);
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdatePopularLocation(UpdatePopularLocationDto popularLocationDto)
+    {
+        bool isSuccess = await _apiClient.UpdateAsync(popularLocationDto);
+        if (isSuccess)
+            return RedirectToAction(nameof(Index));
+        return View();
+    }
+}
