7e17b593d8c04d3f0ff99f21b5b17a20edc4f864
Orçun Aşık <asikorcun@gmail.com>
2023-12-08 20:11:56 +0300

    ApiClient interfaces and classes have been created for api consumption

---
+++ RealEstate.WebUI/ApiClients/Abstracts/IProductApiClient.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate.WebUI.Dtos.ProductDtos;
+
+namespace RealEstate.WebUI.ApiClients.Abstracts;
+
+public interface IProductApiClient
+{
+    Task<List<ResultProductDto>> GetAllProductAsync();
+    Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync();
+    Task<bool> ProductDealOfTheDayStatusChangeToTrueAsync(int id);
+    Task<bool> ProductDealOfTheDayStatusChangeToFalseAsync(int id);
+}
