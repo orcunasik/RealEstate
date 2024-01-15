36e86b7d004584369b9dd4ae75c21e064cc8fe4c
Orçun Aşık <asikorcun@gmail.com>
2024-01-16 00:47:04 +0300

    api endpoint added for ToDoList method

---
+++ RealEstate.Api/Controllers/ToDoListsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.Api.Dtos.ToDoListDtos;
+using RealEstate.Api.Repositories.ToDoListRepository;
+
+namespace RealEstate.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDoListsController : ControllerBase
+{
+    private readonly IToDoListRepository _toDoListRepository;
+
+    public ToDoListsController(IToDoListRepository toDoListRepository)
+    {
+        _toDoListRepository = toDoListRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ToDoList()
+    {
+        List<ResultToDoListDto> toDoList = await _toDoListRepository.GetAllToDoListAsync();
+        return Ok(toDoList);
+    }
+}
