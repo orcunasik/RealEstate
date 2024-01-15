33fb750e954dafc3d553d611be6dbdc638ba1ad6
Orçun Aşık <asikorcun@gmail.com>
2024-01-15 17:28:27 +0300

    Added dtos for Contact model

---
+++ RealEstate.Api/Dtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.Api.Dtos.ContactDtos;
+
+public class CreateContactDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime SendDate { get; set; }
+}
