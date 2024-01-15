92e0f639baf469e1857e92dba9dd2783bbef3f8f
Orçun Aşık <asikorcun@gmail.com>
2024-01-15 18:25:22 +0300

    Added ViewComponent class and partial view page

---
+++ RealEstate.WebUI/Areas/Admin/ViewComponents/Dashboard/_DashboardLastContactsViewComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Dtos.ContactDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.ViewComponents.Dashboard;
+
+public class _DashboardLastContactsViewComponentPartial : ViewComponent
+{
+    private readonly IContactApiClient _contactApiClient;
+
+    public _DashboardLastContactsViewComponentPartial(IContactApiClient contactApiClient)
+    {
+        _contactApiClient = contactApiClient;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        List<LastContactResultDto> lastContacts = await _contactApiClient.GetLastContactsAsync();
+        return View(lastContacts);
+    }
+}
