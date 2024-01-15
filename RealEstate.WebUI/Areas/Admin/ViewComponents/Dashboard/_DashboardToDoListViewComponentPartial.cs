66ac828fb16dd1cb58162120be256facab4706cd
Orçun Aşık <asikorcun@gmail.com>
2024-01-16 01:24:03 +0300

    Added ViewComponent class and partial view page

---
+++ RealEstate.WebUI/Areas/Admin/ViewComponents/Dashboard/_DashboardToDoListViewComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Dtos.ToDoListDtos;
+
+namespace RealEstate.WebUI.Areas.Admin.ViewComponents.Dashboard;
+
+public class _DashboardToDoListViewComponentPartial : ViewComponent
+{
+    private readonly IToDoListApiClient _toDoListApiClient;
+
+    public _DashboardToDoListViewComponentPartial(IToDoListApiClient toDoListApiClient)
+    {
+        _toDoListApiClient = toDoListApiClient;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        List<ResultToDoListDto> toDoList = await _toDoListApiClient.GetAllToDoListAsync();
+        return View(toDoList);
+    }
+}
