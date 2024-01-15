ed52eb132ec9d9f7a325e112d152c3de4c1f5ffa
Orçun Aşık <asikorcun@gmail.com>
2024-01-16 01:23:14 +0300

    ApiClient interfaces and classes have been created for api consumption

---
+++ RealEstate.WebUI/ApiClients/Concretes/ToDoListApiClient.cs
@@ -0,0 +1,22 @@
+﻿using RealEstate.WebUI.ApiClients.Abstracts;
+using RealEstate.WebUI.Configurations;
+using RealEstate.WebUI.Dtos.ToDoListDtos;
+
+namespace RealEstate.WebUI.ApiClients.Concretes;
+
+public class ToDoListApiClient : IToDoListApiClient
+{
+    private readonly IHttpClientFactory _httpClient;
+    private readonly IDomainService _domainService;
+    public ToDoListApiClient(IHttpClientFactory httpClient, IDomainService domainService)
+    {
+        _httpClient = httpClient;
+        _domainService = domainService;
+    }
+    public async Task<List<ResultToDoListDto>> GetAllToDoListAsync()
+    {
+        HttpClient client = _httpClient.CreateClient();
+        List<ResultToDoListDto>? response = await client.GetFromJsonAsync<List<ResultToDoListDto>>(_domainService.Domain() + "api/ToDoLists/");
+        return response;
+    }
+}
