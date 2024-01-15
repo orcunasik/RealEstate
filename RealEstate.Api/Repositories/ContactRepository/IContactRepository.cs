f591ff1f7dfa01e3cf5d6dc553041c346fc09ac9
Orçun Aşık <asikorcun@gmail.com>
2024-01-15 17:30:27 +0300

    Added methods to interface and GetLastContactsAsync method implemented

---
+++ RealEstate.Api/Repositories/ContactRepository/IContactRepository.cs
@@ -0,0 +1,12 @@
+﻿using RealEstate.Api.Dtos.ContactDtos;
+
+namespace RealEstate.Api.Repositories.ContactRepository;
+
+public interface IContactRepository
+{
+    Task<List<ResultContactDto>> GetAllContactAsync();
+    Task<List<LastContactResultDto>> GetLastContactsAsync();
+    Task<GetByIdContactDto> GetContactAsync(int id);
+    Task<CreateContactDto> CreateContactAsync(CreateContactDto contactDto);
+    void DeleteContact(GetByIdContactDto contactDto);
+}
